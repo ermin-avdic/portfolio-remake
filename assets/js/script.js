0a0e5be7cd65d4a709822953c08b451437ffd5e8
Ermin <usb007agent@gmail.com>
2020-01-22 13:48:04 +0100

    navbar resizing transition bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,4 +17,13 @@ const navSlide = () => {
     });
 }
 
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
+
 navSlide();
